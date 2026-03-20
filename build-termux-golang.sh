3720561cd32fb59eddd14f88c52cef487500e3b3
shengshampoo <shengzh@tutanota.com>
2026-03-20 08:46:21 +0000

    Update build-termux-golang.sh

--- build-termux-golang.sh
+++ build-termux-golang.sh
@@ -31,3 +31,24 @@ CGO_ENABLED=1 GOOS=android GOARCH=arm64 go build -pgo=auto -a -ldflags '-w -s'
 
 tar vcJf ./rclone.tar.xz rclone
 cp ./rclone.tar.xz /work/artifact
+
+# q
+cd $WORKSPACE
+git clone https://github.com/natesales/q
+cd q
+CGO_ENABLED=1 GOOS=android GOARCH=arm64 go build -pgo=auto -a -ldflags '-w -s -X main.version=release'
+
+tar vcJf ./q.tar.xz q
+cp ./q.tar.xz /work/artifact
+
+# doggo
+cd $WORKSPACE
+git clone https://github.com/mr-karan/doggo
+cd doggo
+HASH="$(git rev-parse --short HEAD)"
+BUILD_DATE="$(date '+%Y-%m-%d %H:%M:%S')"
+VERSION=$HASH
+CGO_ENABLED=1 GOOS=android GOARCH=arm64 go build -pgo=auto -a -ldflags="-w -s -X 'main.buildVersion=${VERSION}' -X 'main.buildDate=${BUILD_DATE}'" -o ./doggo ./cmd/doggo/
+
+tar vcJf ./doggo.tar.xz doggo
+cp ./doggo.tar.xz /work/artifact
