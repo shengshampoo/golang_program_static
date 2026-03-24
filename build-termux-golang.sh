97d9cb6aaae1a75695ece858786ebfd87e173ec7
shengshampoo <shengzh@tutanota.com>
2026-03-24 12:44:59 +0000

    Update build-termux-golang.sh

--- build-termux-golang.sh
+++ build-termux-golang.sh
@@ -8,7 +8,7 @@ mkdir -p $WORKSPACE
 mkdir -p /work/artifact
 
 # https://github.com/HomuHomu833/android-ndk-custom
-curl -sL https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-musl.tar.xz | tar x --xz
+curl -sL "https://github.com/HomuHomu833/android-ndk-custom/releases/download/r29/android-ndk-r29-$(uname -m)-linux-musl.tar.xz" | tar x --xz
 #curl -LO https://dl.google.com/android/repository/android-ndk-r29-linux.zip && unzip android-ndk-r29-linux.zip
 
 export CC="/android-ndk-r29/toolchains/llvm/prebuilt/linux-$(uname -m)/bin/aarch64-linux-android23-clang"
