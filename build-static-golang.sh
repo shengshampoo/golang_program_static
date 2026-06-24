1b1a8e03db685170b88034ad6af861b4dec125c2
shengshampoo <shengzh@tutanota.com>
2026-06-24 11:55:26 +0000

    Update build-static-golang.sh

--- build-static-golang.sh
+++ build-static-golang.sh
@@ -28,18 +28,10 @@ git clone https://github.com/hmgle/graftcp.git
 cd graftcp
 sed -i '/$(CROSS_COMPILE)/s/^/#&/' ./Makefile
 sed -i '22i#define uint unsigned int' ./cidr-trie.c
-sed -i '23i#define u_char unsigned char' ./cidr-trie.c
+sed -i '23i#define u_char unsigned char' ./cidr-trie.c 
 CFLAGS="$CFLAGS -static" LDFLAGS="-static --static -no-pie -s" make
-cd local
-if [ "$(uname -m)" == "x86_64" ]; then
-GOAMD64=v3 GOOS=$(uname -o | sed -e s@^.*/@@ | tr '[:upper:]' '[:lower:]') GOARCH=amd64 CGO_ENABLED=1 go build -pgo=auto -a -tags netgo -ldflags '-s -w -extldflags "-static"' ./cmd/graftcp-local
-elif [ "$(uname -m)" == "aarch64" ]; then
-GOARM64=v8.0,lse GOOS=$(uname -o | sed -e s@^.*/@@ | tr '[:upper:]' '[:lower:]') GOARCH=arm64 CGO_ENABLED=1 go build -pgo=auto -a -tags netgo -ldflags '-s -w -extldflags "-static"' ./cmd/graftcp-local
-else
-exit 1
-fi
 
 cd ../
-tar vcJf ./graftcp.tar.xz graftcp local/graftcp-local
+tar vcJf ./graftcp.tar.xz graftcp local/graftcp
 cp ./graftcp.tar.xz /work/artifact
 
